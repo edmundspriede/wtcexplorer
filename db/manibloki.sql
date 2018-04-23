f048765b100237cee69dd04d7868ab41a6a10f24
Edmunds Priede <edmunds@kls.lv>
2018-04-23 17:50:27 +0300

    bloku sql

---
+++ db/manibloki.sql
@@ -0,0 +1,22 @@
+SELECT
+
+b.number ,
+
+b.miner,
+
+DATE_FORMAT(FROM_UNIXTIME(b.timestamp), '%e %b %Y %h:%m:%s') AS 'datetime' , 
+
+
+
+
+UNHEX(SUBSTRING(b.data, 3)),
+
+b.data
+
+FROM `blocks` b
+
+WHERE b.miner = '0x3be27a1781bf709b38f7764f9dfc6951dad3050c'
+
+AND UNHEX(SUBSTRING(b.data, 3)) NOT LIKE 'dainis%'
+
+ ;
