44cd7a9dca60070db80a29ae1de9b1fa70466ac8
Edmunds Priede <edmunds@kls.lv>
2018-04-24 14:10:53 +0300

    sync back to one file

--- sync.php
+++ sync.php
@@ -1,5 +1,7 @@
 <?php
 
+  
+    
 
 
 /* script to import blocks from walton RPC
@@ -8,12 +10,36 @@
  * 
  */
 
+define ('CMS_PATH_SEP','\\');
+define ('CMS_BASEPATH',		dirname(__FILE__) . CMS_PATH_SEP);
+define ("CMS_VENDOR_PATH",	CMS_BASEPATH.'vendor'.CMS_PATH_SEP);
 
-require '../config.inc';
-require(CMS_LIBPATH.CMS_SYSPATH."s_init.inc");
-require CMS_LIBPATH.'tables.php';
+
+require './lib/tables.php';
 require CMS_VENDOR_PATH.'ethereum-php-master/ethereum.php';
+require CMS_BASEPATH."lib/d_data.php"; 
+require CMS_BASEPATH."lib/common_functions.php"; 
 
+set_time_limit ( 600 );
+error_reporting (E_ALL & ~E_DEPRECATED);
+ini_set('display_errors', 0);
+
+if(php_sapi_name() == 'cli' && empty($_SERVER['REMOTE_ADDR'])) {
+          define('IS_CMD_LINE',true);
+     } else {
+          define('IS_CMD_LINE',false);
+     }
+
+
+define ('CMS_DBSERVER', '127.0.0.1');
+define ('CMS_DBASE', 'wtc');
+define ('CMS_DBUSER', 'root');
+define ('CMS_DBPASS', '');
+
+
+mysql_connect(CMS_DBSERVER,CMS_DBUSER,CMS_DBPASS) or halt_error(mysql_error());
+mysql_select_db(CMS_DBASE) or halt_error(mysql_error());
+mysql_query("SET NAMES utf8");
 
 
 
