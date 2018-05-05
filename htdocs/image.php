86934bcb8d90ab313d01e653485d25041927a78c
Edmunds Priede <edmunds@kls.lv>
2018-05-05 06:47:29 +0300

    removed cms files

--- htdocs/image.php
+++
@@ -1,8 +0,0 @@
-<?
-require("../config.inc");
-require(CMS_LIBPATH.CMS_SYSPATH."s_init.inc");
-$nId = getNumber('id');
-if (empty($nId)) die('no file specified');
-
-print("<img src='/?mod=903&print=ajax&action=show_file&d_file_id=".$nId."' border='0' />");
-?>
