cd6271681d1b826c7600d276059f3e7f8bebbb21
Edmunds Priede <edmunds@kls.lv>
2018-05-09 06:59:06 +0300

    configuration

---
+++ wtc-grab/config.php
@@ -0,0 +1,16 @@
+<?php
+
+  $MY_DB_SERV = '127.0.0.1'; //MySQL server
+  $MY_DB_USER = 'wtc'; //User
+  $MY_DB_PASS = 'wtc'; //Password
+  $MY_DB_NAME = 'db_wtc'; //Database
+
+  $WLT_HOST = '192.168.10.212';
+  $WLT_PORT = 8545;
+
+  $OUTPUT_TO_LOG = false;
+  $LOG_FILE_NAME = 'wtc-grab.log';
+
+  $LOG_BLK_DATA = false;
+
+?>
